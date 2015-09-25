c14cc9b93d61324afe553d76be1c48b6083d4f9e
MetzOhanian <noah.smith@metzohanian.com>
2015-09-25 17:21:46 -0500

    quieten firmware loading

--- firmware/firmware.0xd.output/installfw.sh
+++ firmware/firmware.0xd.output/installfw.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
+
 make
 for f in *.dts; 
 	do  file="$(echo $f | sed s/-00A0.dts//)"; 
-	echo $file > /sys/devices/bone_capemgr.9/slots; 
+	echo $file > /sys/devices/bone_capemgr.9/slots  2> /dev/null
 done
-cat /sys/devices/bone_capemgr.9/slots
